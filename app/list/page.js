e7374c6dcdca0c8b2334a8780f018e1124bb8f11
juae LEE <leejuae1020@naver.com>
2023-05-04 15:16:33 +0900

    DB데이터 출력해보기

---
+++ app/list/page.js
@@ -0,0 +1,18 @@
+import { connectDB } from "@/util/database";
+
+export default async function List() {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+  console.log(result[0].title);
+
+  return (
+    <div className="list-bg">
+      {result.map((a, i) => (
+        <div className="list-item" key={i}>
+          <h4>{result[i].title} </h4>
+          <p>1월 1일</p>
+        </div>
+      ))}
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -6,7 +6,6 @@ import { MongoClient } from "mongodb";
 export default async function Home() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-  console.log(result);
 
   return <div>안녕!</div>;
 }
