ec5202d6c97ae5a40db1a7345fb0a9f42e0f5dcc
juae LEE <leejuae1020@naver.com>
2023-05-04 15:46:28 +0900

    DetailPage _Dinamic route

--- app/list/page.js
+++ app/list/page.js
@@ -1,15 +1,18 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
 export default async function List() {
   const db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-  console.log(result[0].title);
 
   return (
     <div className="list-bg">
       {result.map((a, i) => (
         <div className="list-item" key={i}>
-          <h4>{result[i].title} </h4>
+          <Link href={"/detail/" + result[i]._id}>
+            {" "}
+            <h4>{result[i].title} </h4>
+          </Link>
           <p>1월 1일</p>
         </div>
       ))}
